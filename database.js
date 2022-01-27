d815df7b7adcb1ab6dbb86f5b98358ba1be33e51
Diego Fernando Gómez <d.fernandogomez94@gmail.com>
2022-01-26 20:17:35 -0500

    implementacion bae de datos separadas para dev y produccion

--- database.js
+++ database.js
@@ -2,9 +2,18 @@ const { connect } =  require('mongoose')
 
 const conexion = async () => {
     try {
+        let db = null;
+        if(process.env.NODE_ENV == 'production') {
 
-        // const db = await connect('mongodb://localhost/dbGym')
-        const db = await connect(process.env.MONGODB_URI)
+            // const db = await connect('mongodb://localhost/dbGym')
+            db = await connect(process.env.MONGODB_URI_PROD)
+
+        } else if(process.env.NODE_ENV == 'development') {
+
+            // const db = await connect('mongodb://localhost/dbGym')
+            db = await connect(process.env.MONGODB_URI_DEV)
+
+        }
         // const db = await connect('mongodb+srv://diego:gomez@cluster0.rhwax.mongodb.net/dbGym?retryWrites=true&w=majority')
         console.log('Nombre de la conexion: ', db.connection.name)
         console.log('conexión exitosa!.')
